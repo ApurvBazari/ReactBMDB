2e149bb08f7fb63f2e99809a0f54a3b495e9c47b
Apurv Bazari <apurv.bazari@bookmyshow.com>
2017-09-21 20:53:33 +0530

    Filter updated

--- src/components/component/filterForm.js
+++ src/components/component/filterForm.js
@@ -1,7 +1,7 @@
 'use strict'
 
 import React from 'react'
-import {Form, FormGroup, Col, FormControl, Button} from 'react-bootstrap'
+import {Radio, MenuItem, Form, FormGroup, Col, FormControl, Button, Checkbox, Row, ControlLabel, DropdownButton} from 'react-bootstrap'
 
 class Filter extends React.Component{
 	toggleFilter = () => {
@@ -13,18 +13,69 @@ class Filter extends React.Component{
 	}
 
 	render() {
+		let genreList = [];
+		this.props.genres.forEach((genre) => {
+			genreList.push(<Checkbox title={genre.name} key={genre.id}>{genre.name}</Checkbox>)
+		});
+		let years = [2017,2016,2015,2014,2013,2012,2011,2010];
+		let yearList = [];
+		years.forEach((year) => {
+			yearList.push(<Radio key={year}>{year}</Radio>)
+		});
 		return (
-			<Form>
-				<FormGroup>
+			<Form horizontal>
+				<FormGroup name="genreGroup">
 					<Col sm={2}>
-						Genre type
+						<ControlLabel>
+							Genre type
+						</ControlLabel>
 					</Col>
 					<Col sm={10}>
-						<FormControl type="checkbox" value="Check" />
+						{genreList}
 					</Col>
 				</FormGroup>
-				<Button onClick={this.filterData.bind(this)}>Filter Results</Button>
-				<Button onClick={this.toggleFilter.bind(this)}>Close Filter</Button>
+				<FormGroup name="certificationGroup">
+					<Col sm={2}>
+						<ControlLabel>
+							Year
+						</ControlLabel>
+					</Col>
+					<Col sm={10}>
+						{yearList}
+					</Col>
+				</FormGroup>
+				<FormGroup>
+					<Col sm={2}>
+						<ControlLabel>
+							Sort by
+						</ControlLabel>
+					</Col>
+					<DropdownButton bsStyle='default' title='Sort by' id='sortByFilter'>
+						<MenuItem>Rating: High to Low</MenuItem>
+						<MenuItem>Rating: Low to High</MenuItem>
+						<MenuItem divider />
+						<MenuItem>Popularity: High to Low</MenuItem>
+						<MenuItem>Popularity: Low to Highgh</MenuItem>
+						<MenuItem divider />
+						<MenuItem>Release date: High to Low</MenuItem>
+						<MenuItem>Release date: Low to High</MenuItem>
+						<MenuItem divider />
+						<MenuItem>Revenue: High to Low</MenuItem>
+						<MenuItem>Revenue: Low to High</MenuItem>
+						<MenuItem divider />
+						<MenuItem>Vote Average: High to Low</MenuItem>
+						<MenuItem>Vote Average: Low to High</MenuItem>
+						<MenuItem divider />
+						<MenuItem>Vote Count: High to Low</MenuItem>
+						<MenuItem>Vote Count: Low to High</MenuItem>
+					</DropdownButton>
+				</FormGroup>
+				<FormGroup className="filterButtons">
+					<Col sm={12}>
+					<Button bsStyle="primary" onClick={this.filterData.bind(this)}>Filter Results</Button>
+					<Button bsStyle="danger" onClick={this.toggleFilter.bind(this)}>Close Filter</Button>
+					</Col>
+				</FormGroup>
 			</Form>
 		)
 	}

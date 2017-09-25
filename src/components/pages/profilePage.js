b03ce70935e38896bb1b65b68a0234bd0fc3c78d
Apurv Bazari <apurv.bazari@bookmyshow.com>
2017-09-26 01:51:51 +0530

    Added footer css

--- src/components/pages/profilePage.js
+++ src/components/pages/profilePage.js
@@ -7,10 +7,6 @@ import {bindActionCreators} from 'redux'
 import {itemsFetchData} from '../../actions/fetchItems.js'
 
 class Profile extends React.Component{
-	componentWillMount = () => {
-
-	}
-
 	render () {
 		return (
 			<h1>Profile Details</h1>
